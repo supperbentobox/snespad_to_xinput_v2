f8489538855c3ff0b87ba5966f3292b8a0998c8b
supperbentobox <156239854+supperbentobox@users.noreply.github.com>
2024-12-29 00:20:55 -0300

    Update snespad_to_xinput.ino
    Add Home Button with Start + Select
    
    Start is now -> L2 
    
    and  Select is now -> Start

--- snespad_to_xinput.ino
+++ snespad_to_xinput.ino
@@ -1,4 +1,4 @@
-/*
+ /*
   ******************************************************************************
   SNES Controller to XInput Converter for Arduino Leonardo(Pro Micro)
   ------------------------------------------------------------------------------
@@ -73,41 +73,41 @@
 #include <XInput.h>
 
 // Arduino pins
-#define DATA_CLOCK  7  // Wiring SNES Data Clock
-#define DATA_LATCH  8  // Wiring SNES Data Latch
+#define DATA_CLOCK 7   // Wiring SNES Data Clock
+#define DATA_LATCH 8   // Wiring SNES Data Latch
 #define DATA_SERIAL 9  // Wiring SNES Data Serial
 
 // SNES buttons index
-#define SNES_BUTTON_B      0
-#define SNES_BUTTON_Y      1
-#define SNES_BUTTON_SEL    2
-#define SNES_BUTTON_ST     3
-#define SNES_BUTTON_UP     4
-#define SNES_BUTTON_DOWN   5
-#define SNES_BUTTON_LEFT   6
-#define SNES_BUTTON_RIGHT  7
-#define SNES_BUTTON_A      8
-#define SNES_BUTTON_X      9
-#define SNES_BUTTON_L     10
-#define SNES_BUTTON_R     11
+#define SNES_BUTTON_B 0
+#define SNES_BUTTON_Y 1
+#define SNES_BUTTON_SEL 2
+#define SNES_BUTTON_ST 3
+#define SNES_BUTTON_UP 4
+#define SNES_BUTTON_DOWN 5
+#define SNES_BUTTON_LEFT 6
+#define SNES_BUTTON_RIGHT 7
+#define SNES_BUTTON_A 8
+#define SNES_BUTTON_X 9
+#define SNES_BUTTON_L 10
+#define SNES_BUTTON_R 11
 
 // Data store for current state of SNES buttons
 int buttons[12];
 
 void setup() {
   // Set DATA_CLOCK normally HIGH
-  pinMode (DATA_CLOCK, OUTPUT);
-  digitalWrite (DATA_CLOCK, HIGH);
-  
+  pinMode(DATA_CLOCK, OUTPUT);
+  digitalWrite(DATA_CLOCK, HIGH);
+
   // Set DATA_LATCH normally LOW
-  pinMode (DATA_LATCH, OUTPUT);
-  digitalWrite (DATA_LATCH, LOW);
-  
+  pinMode(DATA_LATCH, OUTPUT);
+  digitalWrite(DATA_LATCH, LOW);
+
   // Set DATA_SERIAL normally HIGH
-  pinMode (DATA_SERIAL, OUTPUT);
-  digitalWrite (DATA_SERIAL, HIGH);
-  pinMode (DATA_SERIAL, INPUT_PULLUP);  
-  
+  pinMode(DATA_SERIAL, OUTPUT);
+  digitalWrite(DATA_SERIAL, HIGH);
+  pinMode(DATA_SERIAL, INPUT_PULLUP);
+
   // Initialize XInput Library
   XInput.setJoystickRange(-1, 1);
   XInput.begin();
@@ -117,44 +117,55 @@ void loop() {
   // Latch
   digitalWrite(DATA_LATCH, HIGH);
   digitalWrite(DATA_LATCH, LOW);
-  
+
   // Read data bit by bit from SR
   for (int i = 0; i < 16; i++) {
-    digitalWrite (DATA_CLOCK, LOW);
+    digitalWrite(DATA_CLOCK, LOW);
     if (i <= 11) {
-      buttons[i] = digitalRead (DATA_SERIAL);
+      buttons[i] = digitalRead(DATA_SERIAL);
     }
-    digitalWrite (DATA_CLOCK, HIGH);
+    digitalWrite(DATA_CLOCK, HIGH);
   }
-  
+
   // -----------------------------------------
   // Set Joystick buttons state
   // -----------------------------------------
   // Directional Pad
-  if (buttons[SNES_BUTTON_UP] == 0){
+  if (buttons[SNES_BUTTON_UP] == 0) {
     XInput.setJoystickY(JOY_LEFT, 1);
-  } else if (buttons[SNES_BUTTON_DOWN] == 0){
+  } else if (buttons[SNES_BUTTON_DOWN] == 0) {
     XInput.setJoystickY(JOY_LEFT, -1);
   } else {
     XInput.setJoystickY(JOY_LEFT, 0);
   }
-  if (buttons[SNES_BUTTON_RIGHT] == 0){
+
+  if (buttons[SNES_BUTTON_RIGHT] == 0) {
     XInput.setJoystickX(JOY_LEFT, 1);
-  } else if (buttons[SNES_BUTTON_LEFT] == 0){
+  } else if (buttons[SNES_BUTTON_LEFT] == 0) {
     XInput.setJoystickX(JOY_LEFT, -1);
   } else {
     XInput.setJoystickX(JOY_LEFT, 0);
   }
-  
-  // Buttons
-  XInput.setButton(BUTTON_A,     !buttons[SNES_BUTTON_A]);
-  XInput.setButton(BUTTON_B,     !buttons[SNES_BUTTON_B]);
-  XInput.setButton(BUTTON_X,     !buttons[SNES_BUTTON_X]);
-  XInput.setButton(BUTTON_Y,     !buttons[SNES_BUTTON_Y]);
-  XInput.setButton(BUTTON_LB,    !buttons[SNES_BUTTON_L]);
-  XInput.setButton(BUTTON_RB,    !buttons[SNES_BUTTON_R]);
-  XInput.setButton(BUTTON_BACK,  !buttons[SNES_BUTTON_SEL]);
-  XInput.setButton(BUTTON_START, !buttons[SNES_BUTTON_ST]);
+
+  // Buttons  swapeados para: snes -> xbox - > psx
+  XInput.setButton(BUTTON_A, !buttons[SNES_BUTTON_B]);
+  XInput.setButton(BUTTON_B, !buttons[SNES_BUTTON_A]);
+  XInput.setButton(BUTTON_X, !buttons[SNES_BUTTON_Y]);
+  XInput.setButton(BUTTON_Y, !buttons[SNES_BUTTON_X]);
+  XInput.setButton(BUTTON_LB, !buttons[SNES_BUTTON_L]);
+  XInput.setButton(BUTTON_RB, !buttons[SNES_BUTTON_R]);
+
+  if (!buttons[SNES_BUTTON_ST] && !buttons[SNES_BUTTON_SEL] ) {
+    XInput.setButton(BUTTON_LOGO, 1);
+    XInput.setButton(BUTTON_START, 0);
+    XInput.setButton(TRIGGER_RIGHT, 0);
+  } else {
+    XInput.setButton(BUTTON_LOGO, 0);
+    XInput.setButton(BUTTON_START, !buttons[SNES_BUTTON_SEL]);
+    //XInput.setButton(BUTTON_BACK,    !buttons[SNES_BUTTON_SEL]);
+    XInput.setButton(TRIGGER_RIGHT, !buttons[SNES_BUTTON_ST]);
+    //XInput.setTrigger(TRIGGER_RIGHT, !buttons[SNES_BUTTON_SEL]);
+  }
 
   delay(10);
 }
